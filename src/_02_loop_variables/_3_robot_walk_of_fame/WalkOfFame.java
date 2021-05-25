b005e8f74d41da3604f480c783970fef70790521
student <student@ip-172-31-58-241.us-west-2.compute.internal>
2021-05-24 18:23:24 -0700

    bottles.pde

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -10,23 +10,30 @@ public class WalkOfFame {
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 mack.setX(50);
+mack.setSpeed(100);
 		// 2. Make the robot draw a star shape. Hint: angle=144.
 mack.penDown();
-mack.move(30);
-mack.turn(144);
-mack.move(30);
-mack.turn(144);
-mack.move(30);
-mack.turn(144);
+
+for (int mackAsInt=0; mackAsInt < 10; mackAsInt++) {
+	for (int mackAsInt2=0; mackAsInt2 < 5; mackAsInt2++) {
+		mack.move(30);
+		mack.turn(144);
+	}
+	mack.setAngle(90);
+	mack.penUp();
+	mack.move(50);
+	mack.penDown();
+}
 
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of 10 stars (see recipe to see how it should look)
-		 * 
+	       
 		 * Hint: The distance between stars is 50.
 		 */
+
 mack.hide();
 	}
 
